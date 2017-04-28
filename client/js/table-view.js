153afb7063f2cecd859e0ccb64d203b4f5f89954
lichinshao <lichinshao@gmail.com>
2017-04-28 12:44:22 -0700

    modified table view

--- client/js/table-view.js
+++ client/js/table-view.js
@@ -1,5 +1,5 @@
 const { getLetterRange } = require('./array-util');
-const { removeChildren, createTH } = require('./dom-util');
+const { removeChildren, createTH, createTR, createTD } = require('./dom-util');
 
 
 class TableView {
@@ -14,10 +14,12 @@ class TableView {
 
 	initDomReferences() {
 		this.headerRowEl = document.querySelector('THEAD TR');
+		this.sheetBodyEl = document.querySelector('TBODY');
 	}
 
 	renderTable() {
 		this.renderTableHeader();
+		this.renderTableBody();
 	}
 
 	renderTableHeader() {
@@ -25,6 +27,22 @@ class TableView {
 		getLetterRange('A', this.model.numCols)
 			.map(colLabel => createTH(colLabel))
 			.forEach(th => this.headerRowEl.appendChild(th));
+	}
+
+	renderTableBody() {
+		const fragment = document.createDocumentFragment();
+		for (let row = 0; row < this.model.numRows; row++) {
+			const tr = createTR();
+			for (let col = 0; col < this.model.numCols; col++) {
+				const position = {col :col, row: row};
+				const value = this.model.getValue(position);
+				const td = createTD(value);
+				tr.appendChild(td);
+			}
+			fragment.appendChild(tr);
+		}
+		removeChildren(this.sheetBodyEl);
+		this.sheetBodyEl.appendChild(fragment);
 	}
 }
 
--- public/js/bundle.js
+++ public/js/bundle.js
@@ -75,7 +75,7 @@ class TableModel {
 module.exports = TableModel;
 },{}],5:[function(require,module,exports){
 const { getLetterRange } = require('./array-util');
-const { removeChildren, createTH } = require('./dom-util');
+const { removeChildren, createTH, createTR, createTD } = require('./dom-util');
 
 
 class TableView {
@@ -90,10 +90,12 @@ class TableView {
 
 	initDomReferences() {
 		this.headerRowEl = document.querySelector('THEAD TR');
+		this.sheetBodyEl = document.querySelector('TBODY');
 	}
 
 	renderTable() {
 		this.renderTableHeader();
+		this.renderTableBody();
 	}
 
 	renderTableHeader() {
@@ -101,6 +103,22 @@ class TableView {
 		getLetterRange('A', this.model.numCols)
 			.map(colLabel => createTH(colLabel))
 			.forEach(th => this.headerRowEl.appendChild(th));
+	}
+
+	renderTableBody() {
+		const fragment = document.createDocumentFragment();
+		for (let row = 0; row < this.model.numRows; row++) {
+			const tr = createTR();
+			for (let col = 0; col < this.model.numCols; col++) {
+				const position = {col :col, row: row};
+				const value = this.model.getValue(position);
+				const td = createTD(value);
+				tr.appendChild(td);
+			}
+			fragment.appendChild(tr);
+		}
+		removeChildren(this.sheetBodyEl);
+		this.sheetBodyEl.appendChild(fragment);
 	}
 }
 
